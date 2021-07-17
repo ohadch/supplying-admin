c23bb0d3cbac1195b98f799db67340b95496b0ce
Ohad <ohad@salaryo.com>
2021-07-17 11:24:25 +0300

    Fixes bugs in OfferItem.inputs.ts

--- server/src/models/OfferItem/OfferItem.inputs.ts
+++ server/src/models/OfferItem/OfferItem.inputs.ts
@@ -1,20 +1,35 @@
-import {Field, Float, InputType} from "type-graphql";
+import {Field, Float, InputType, Int} from "type-graphql";
 import {OfferItem} from "./OfferItem.model";
 
 @InputType()
 export class CreateOfferItemInput implements Partial<OfferItem>{
     @Field(type => Float)
-    priceOfferedByTheOriginator: number;
+    pricePerUnitOffered: number;
 
     @Field(type => Float, { nullable: true })
-    priceAgreedByTheOfferee: number;
+    pricePerUnitAgreed: number;
 
     @Field(type => String)
     productId: string;
+
+    @Field(type => String)
+    offerRoundId: string;
+
+    @Field(type => Int)
+    quantity: number;
+
+    @Field(type => Float, { nullable: true })
+    discountPercent: number;
 }
 
 @InputType()
 export class UpdateOfferItemInput implements Partial<OfferItem>{
-    @Field(type => Float)
-    priceAgreedByTheOfferee: number;
+    @Field(type => Float, { nullable: true })
+    pricePerUnitAgreed: number;
+
+    @Field(type => Int, { nullable: true })
+    quantity: number;
+
+    @Field(type => Float, { nullable: true })
+    discountPercent: number;
 }
